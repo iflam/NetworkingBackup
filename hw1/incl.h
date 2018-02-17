71fe9791ffd50ea10ab0837f855dc3e56282605d
benito <bkestelman@outlook.com>
2018-02-17 18:30:32 -0500

    one-way communication between parent and child in xterm

---
+++ hw1/incl.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/select.h>
+#include<arpa/inet.h>
+
+#include "errmsg.h"
