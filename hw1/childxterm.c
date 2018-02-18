d8f55d780b001006535d9a79de3b047d1b1939ff
benito <bkestelman@outlook.com>
2018-02-17 20:09:43 -0500

    two-way pipes with xterm and select

--- hw1/childxterm.c
+++ hw1/childxterm.c
@@ -5,11 +5,53 @@
 int main(int argc, char *argv[]) {
 	char sendline[MAXLINE], recvline[MAXLINE];
 
-	while(1) {
-		int n = read(atoi(argv[1]), recvline, MAXLINE);
-		recvline[n] = 0; // null-terminate
-		printf("(child) received msg:\n");
-		fputs(recvline, stdout);
-	}
+	int pipefd[2];
+	pipefd[0] = atoi(argv[1]);
+	pipefd[1] = atoi(argv[2]);
+	printf("write end %d\n", pipefd[1]);
 
+	fd_set rfds, rfds_init;
+
+	int maxfd = pipefd[0];
+
+	FD_ZERO(&rfds_init);
+	FD_SET(fileno(stdin), &rfds_init);
+	FD_SET(pipefd[0], &rfds_init);
+
+	while(1) {
+		rfds = rfds_init;
+		if(select(maxfd+1, &rfds, NULL, NULL, NULL) < 0)
+			err_sys("select error\n");
+		if(FD_ISSET(fileno(stdin), &rfds)) { 
+			fgets(sendline, MAXLINE, stdin); 
+			if(write(pipefd[1], sendline, strlen(sendline)) <= 0)
+				perror("write error"); 
+		}
+		if(FD_ISSET(pipefd[0], &rfds)) { 
+			if(read(pipefd[0], recvline, MAXLINE) == 0)
+				err_quit("EOF\n"); 
+			fputs(recvline, stdout);
+		}
+	}
+/* SELECT
+	fd_set rfds, rfds_init;
+
+	FD_ZERO(&rfds_init);
+	FD_SET(fileno(stdin), &rfds_init);
+	FD_SET(sockfd, &rfds_init);
+
+	while(1) {
+		rfds = rfds_init;
+		if(select(sockfd+1, &rfds, NULL, NULL, NULL) == -1)
+			err_sys("select error\n");
+		if(FD_ISSET(fileno(stdin), &rfds)) { 
+			fgets(sendline, MAXLINE, stdin); // read stdin
+			write(sockfd, sendline, strlen(sendline)); // send to sockfd
+		}
+		if(FD_ISSET(sockfd, &rfds)) { 
+			if(read(sockfd, recvline, MAXLINE) == 0)
+				err_quit("EOF\n"); 
+			fputs(recvline, stdout);
+		}
+	}*/
 }
--- hw1/parentxterm.c
+++ hw1/parentxterm.c
@@ -5,31 +5,77 @@
 int main() {
 	char sendline[MAXLINE], recvline[MAXLINE];
 
-	int pipefd[2];
+	int client2chat[2], chat2client[2]; //pipes
 
-	pipe(pipefd);
+	pipe(client2chat);
+	pipe(chat2client);
 
 	pid_t pid = fork();
 
-	if(pid == 0) { // child 
-		close(pipefd[1]);
-		char *args[5];
+	if(pid == 0) { // child process
+		close(client2chat[1]);
+		close(chat2client[0]);
+
+		char *args[6];
 		args[0] = "xterm";
 		args[1] = "-e";
-		args[2] = "./child";
+		args[2] = "./child"; // xterm will run child program 
 		args[3] = calloc(sizeof(int)+1, sizeof(char));
-		args[4] = NULL;
-		snprintf(args[3], sizeof(int)+1, "%d", pipefd[0]); // give child pipefd as arg
-		if(execvp(args[0], args) < 0)
+		args[4] = calloc(sizeof(int)+1, sizeof(char));
+		args[5] = NULL;
+		snprintf(args[3], sizeof(int)+1, "%d", client2chat[0]); // give child read end of client2chat pipe 
+		snprintf(args[4], sizeof(int)+1, "%d", chat2client[1]); // give child write end of chat2client pipe 
+
+		if(execvp(args[0], args) < 0) // run xterm
 			perror("exec error");
-		
 	}
-	else {
-		close(pipefd[0]);
+	else { // parent process
+		close(client2chat[0]);
+		close(chat2client[1]);
+
+		fd_set rfds, rfds_init;
+
+		int maxfd = chat2client[0];
+
+		FD_ZERO(&rfds_init);
+		FD_SET(fileno(stdin), &rfds_init);
+		FD_SET(chat2client[0], &rfds_init);
+
 		while(1) {
-			printf("(parent) write a msg:\n");
-			fgets(sendline, MAXLINE, stdin);
-			write(pipefd[1], sendline, strlen(sendline));
+			rfds = rfds_init;
+			if(select(maxfd+1, &rfds, NULL, NULL, NULL) < 0)
+				err_sys("select error");
+			if(FD_ISSET(fileno(stdin), &rfds)) {
+				fgets(sendline, MAXLINE, stdin);
+				write(client2chat[1], sendline, strlen(sendline));
+			}
+			if(FD_ISSET(chat2client[0], &rfds)) {
+				if(read(chat2client[0], recvline, MAXLINE) == 0)
+					err_quit("EOF\n");
+				fputs(recvline, stdout);
+			}
 		}
 	}
+
+/* SELECT
+	fd_set rfds, rfds_init;
+
+	FD_ZERO(&rfds_init);
+	FD_SET(fileno(stdin), &rfds_init);
+	FD_SET(sockfd, &rfds_init);
+
+	while(1) {
+		rfds = rfds_init;
+		if(select(sockfd+1, &rfds, NULL, NULL, NULL) == -1)
+			err_sys("select error\n");
+		if(FD_ISSET(fileno(stdin), &rfds)) { 
+			fgets(sendline, MAXLINE, stdin); // read stdin
+			write(sockfd, sendline, strlen(sendline)); // send to sockfd
+		}
+		if(FD_ISSET(sockfd, &rfds)) { 
+			if(read(sockfd, recvline, MAXLINE) == 0)
+				err_quit("EOF\n"); 
+			fputs(recvline, stdout);
+		}
+	}*/
 }
