1400cebc55f126a673898533869879bd8d459c29
benito <bkestelman@outlook.com>
2018-03-27 11:37:01 -0400

    one print function for all protocols

--- hw2/readPackets.py
+++ hw2/readPackets.py
@@ -10,34 +10,39 @@ def readPacket(sock):
 # protocol is of type Struct (i.e. something we define in structs.py)
 def printPacket(parsed_packet, protocol):
     print(TYPE_STR[protocol])
-    if protocol == Ethernet:
-        print('mac_src', parsed_packet['mac_src'].hex())
-        print('mac_dest', parsed_packet['mac_dest'].hex())
-        print('type', parsed_packet['type'].hex())
-    elif protocol == IP:
-        print('version', parsed_packet['version_hl'].hex())
-        print('length', parsed_packet['total_length'].hex())
-        print('protocol',parsed_packet['protocol'].hex())
-        print('destip',parsed_packet['dest_ip'].hex())
-    elif protocol == DNS:
-        pass # print DNS fields
-    elif protocol == ICMP:
-        pass # print ICMP fields
-    elif protocol == UDP:
-        print('src_port', parsed_packet['src_port'].hex())
-        print('dest_port', parsed_packet['dest_port'].hex())
-        print('length', parsed_packet['length'].hex())
-        print('checksum', parsed_packet['checksum'].hex())
-    elif protocol == TCP:
-        print('src_port', parsed_packet['src_port'].hex())
-        print('dest_port', parsed_packet['dest_port'].hex())
-        print('sequence number', parsed_packet['sequence_number'].hex())
-        print('ack_number', parsed_packet['ack_number'].hex())
-        print('doff_res_ns', parsed_packet['doff_res_ns'].hex())
-        print('flags', parsed_packet['flags'].hex())
-        print('window_size', parsed_packet['window_size'].hex())
-        print('checksum', parsed_packet['checksum'].hex())
-        print('urg_ptr', parsed_packet['urg_ptr'].hex())
+    printFields(parsed_packet)
+#    if protocol == Ethernet:
+#        print('mac_src', parsed_packet['mac_src'].hex())
+#        print('mac_dest', parsed_packet['mac_dest'].hex())
+#        print('type', parsed_packet['type'].hex())
+#    elif protocol == IP:
+#        print('version', parsed_packet['version_hl'].hex())
+#        print('length', parsed_packet['total_length'].hex())
+#        print('protocol',parsed_packet['protocol'].hex())
+#        print('destip',parsed_packet['dest_ip'].hex())
+#    elif protocol == DNS:
+#        pass # print DNS fields
+#    elif protocol == ICMP:
+#        pass # print ICMP fields
+#    elif protocol == UDP:
+#        print('src_port', parsed_packet['src_port'].hex())
+#        print('dest_port', parsed_packet['dest_port'].hex())
+#        print('length', parsed_packet['length'].hex())
+#        print('checksum', parsed_packet['checksum'].hex())
+#    elif protocol == TCP:
+#        print('src_port', parsed_packet['src_port'].hex())
+#        print('dest_port', parsed_packet['dest_port'].hex())
+#        print('sequence number', parsed_packet['sequence_number'].hex())
+#        print('ack_number', parsed_packet['ack_number'].hex())
+#        print('doff_res_ns', parsed_packet['doff_res_ns'].hex())
+#        print('flags', parsed_packet['flags'].hex())
+#        print('window_size', parsed_packet['window_size'].hex())
+#        print('checksum', parsed_packet['checksum'].hex())
+#        print('urg_ptr', parsed_packet['urg_ptr'].hex())
+
+def printFields(parsed_packet):
+    for key in parsed_packet.keys():
+        print(key, ':', parsed_packet[key].hex())
 
 # TODO 
 def stripEthernet(packet):
