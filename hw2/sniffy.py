70b51022e078726c6ca5e9d2dc0f7331e89a880a
benito <bkestelman@outlook.com>
2018-03-31 18:59:29 -0400

    fix everything

--- hw2/sniffy.py
+++ hw2/sniffy.py
@@ -49,7 +49,7 @@ if __name__=='__main__':
             if args_dict['timeout']:
                 if time.time() > start + int(args_dict['timeout']):
                     endSniff()
-        	#Receive Packet
+                #Receive Packet
             packet = readPacket(sock) 
             originalPacket = packet
             if args_dict['hexdump']:
@@ -58,16 +58,19 @@ if __name__=='__main__':
             ethernet_packet = Ethernet.parse(packet)
             if not args_dict['filter'] or args_dict['filter'] == 'Ethernet':
                 printPacket(ethernet_packet, Ethernet) # all packets have Ethernet on top
+                epString += getEpBlock(packet)
             try:
-            	protocol = ETHER_TYPE[ethernet_packet['type']]
+                protocol = ETHER_TYPE[ethernet_packet['type']]
             except:
-            	continue
+                continue
             packet = stripHeader(packet, Ethernet.sizeof()) # now we are interested in the rest of the packet
             
             #parse IP
             ip_packet = IP.parse(packet)
             if not args_dict['filter'] or args_dict['filter'] == 'IP':
-            	printPacket(ip_packet, IP)
+                printPacket(ip_packet, IP)
+                epString += getEpBlock(packet)
+                #epString += getEpBlock(packet)
             packet = stripHeader(packet, IP.sizeof()) #now we are interested in the rest of the stuff
 
             #parse TCP/UDP/ICMP
@@ -75,17 +78,21 @@ if __name__=='__main__':
             transport_packet = ip_type.parse(packet)
             if not args_dict['filter'] or args_dict['filter'] == TYPE_STR[ip_type]:
                 printPacket(transport_packet, ip_type)
+                epString += getEpBlock(packet)
                 #printpcap("hi",packet,ip_type)
             packet = stripHeader(packet, ip_type.sizeof())
+
             #DNS
             if ip_type == UDP and (not args_dict['filter'] or args_dict['filter'] == 'DNS'):
-            	data_packet = DNS.parse(packet)
-            	printPacket(data_packet, DNS) # print DNS!
-            	#if transport_packet['dest_port'] == b"\x00\x35" or transport_packet['src_port'] == b"\x00\x35":
-            	packet = stripHeader(packet,ip_type.sizeof())
-            if not args_dict['filter'] or args_dict['filter'] == TYPE_STR[DNS]:
-                #print("Added")
-                epString+=getEpBlock(packet)
+                data_packet = DNS.parse(packet)
+                printPacket(data_packet, DNS) # print DNS!
+                #if transport_packet['dest_port'] == b"\x00\x35" or transport_packet['src_port'] == b"\x00\x35":
+                #packet = stripHeader(packet,ip_type.sizeof())
+                epString += getEpBlock(packet)
+
+            #if not args_dict['filter'] or args_dict['filter'] == TYPE_STR[DNS]:
+            #    epString+=getEpBlock(packet)
+
             #if not args_dict['filter'] or args_dict['filter'] == protocol:
             #    printPacket(packet, protocol)
             
